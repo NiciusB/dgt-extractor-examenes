095845a6c76dc3976df1032dbcc47fbe107a26cc
NiciusB <nuno@balbona.me>
2020-08-31 21:27:57 +0200

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+	env: {
+		node: true,
+	},
+	extends: [
+		'plugin:@typescript-eslint/recommended',
+		'prettier/@typescript-eslint',
+		'plugin:prettier/recommended',
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 2018,
+		sourceType: 'module',
+	},
+	plugins: ['@typescript-eslint'],
+	rules: {
+		'no-console': ['error'],
+	},
+}
---
+++ src/index.ts
@@ -0,0 +1,53 @@
+import getDgtCuestionario, { DgtPregunta } from './getDgtCuestionario'
+import * as fs from 'fs'
+import * as path from 'path'
+import * as util from 'util'
+
+const writeFile = util.promisify(fs.writeFile)
+
+const storePath = path.join(__dirname, '..', 'store.json')
+
+let store: DgtPregunta[] = []
+loadStore()
+
+setInterval(addRandomQuestionarieToStore, 1000 * 60)
+
+async function addRandomQuestionarieToStore() {
+	const cuestionario = await getDgtCuestionario()
+	let foundNewQuestions = false
+
+	cuestionario.preguntas.forEach((pregunta) => {
+		const alreadyInStore = store.some(
+			(_pregunta) => _pregunta.id === pregunta.id
+		)
+		if (alreadyInStore) return
+
+		foundNewQuestions = true
+		store.push(pregunta)
+	})
+
+	if (foundNewQuestions) {
+		log(`Found new questions on questionaire ${cuestionario.id}`)
+		await saveStore()
+	}
+}
+
+function log(...stuff: unknown[]) {
+	// eslint-disable-next-line no-console
+	console.log(new Date(), ...stuff)
+}
+
+function loadStore() {
+	if (fs.existsSync(storePath)) {
+		try {
+			store = JSON.parse(fs.readFileSync(storePath, 'utf-8'))
+			log(`${store.length} preguntas cargadas`)
+		} catch (err) {
+			throw new Error('Corrupted store.json')
+		}
+	}
+}
+
+function saveStore() {
+	return writeFile(storePath, JSON.stringify(store), 'utf-8')
+}
